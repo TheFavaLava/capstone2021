8eedb026837f0fc918f3f2fbab4ea9c2e894349e
Colby Hillman <colby.hillman@gmail.com>
2021-03-23 16:43:55 -0400

    Changed workgen bash file to prepare for Vagrant

--- scripts/workgen.sh
+++ scripts/workgen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd /home/cs334/capstone2021
-source .venv/bin/activate
-python src/c21server/work_gen/basic_work_gen.py
+cd /home/vagrant/capstone2021
+.venv/bin/python src/c21server/work_gen/basic_work_gen.py
