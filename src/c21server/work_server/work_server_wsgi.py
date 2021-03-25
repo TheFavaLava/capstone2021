915c9c1f2538edda758d5f22e1cabc231a335ff4
Colby Hillman <colby.hillman@gmail.com>
2021-03-25 17:12:32 -0400

    Refactored wsgi for work server

--- src/c21server/work_server/work_server_wsgi.py
+++ src/c21server/work_server/work_server_wsgi.py
@@ -2,8 +2,4 @@ from work_server import app
 import redis
 
 if __name__ == "__main__":
-    server = create_server(redis.Redis())
-    if server is None:
-        print('There is no Redis database to connect to.')
-    else:
-        app.run()
+    app.run()
