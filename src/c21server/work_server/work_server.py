aeedfe443147b19add836956cd4c3add31b2bb5a
Colby Hillman <colby.hillman@gmail.com>
2021-03-25 15:44:27 -0400

    Updated work server file to work with NGINX

--- src/c21server/work_server/work_server.py
+++ src/c21server/work_server/work_server.py
@@ -95,11 +95,3 @@ def create_server(database):
         return jsonify({'client_id': values[0]}), 200
 
     return workserver
-
-
-if __name__ == '__main__':
-    server = create_server(redis.Redis())
-    if server is None:
-        print('There is no Redis database to connect to.')
-    else:
-        server.app.run(host='0.0.0.0', port=8080, debug=False)
