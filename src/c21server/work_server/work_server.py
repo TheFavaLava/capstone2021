db3d391c6cd0d151c4d428705a75e307d0fdb223
Colby Hillman <colby.hillman@gmail.com>
2021-03-25 18:29:37 -0400

    Changed references to redis

--- src/c21server/work_server/work_server.py
+++ src/c21server/work_server/work_server.py
@@ -33,30 +33,30 @@ def check_for_database(func):
 
 @check_for_database
 def get_job(workserver):
-    keys = workserver.redis.hkeys('jobs_waiting')
+    keys = workserver.hkeys('jobs_waiting')
     if len(keys) == 0:
         return False, jsonify({'error': 'There are no jobs available'}), 400
-    value = workserver.redis.hget('jobs_waiting', keys[0])
-    workserver.redis.hset('jobs_in_progress', keys[0], value)
-    workserver.redis.hdel('jobs_waiting', keys[0])
+    value = workserver.hget('jobs_waiting', keys[0])
+    workserver.hset('jobs_in_progress', keys[0], value)
+    workserver.hdel('jobs_waiting', keys[0])
     return True, keys[0].decode(), value.decode()
 
 
 @check_for_database
 def put_results(workserver, data):
     key = get_first_key(data)
-    value = workserver.redis.hget('jobs_in_progress', key)
+    value = workserver.hget('jobs_in_progress', key)
     if value is None:
         body = {'error': 'The job being completed was not in progress'}
         return False, jsonify(body), 400
-    workserver.redis.hdel('jobs_in_progress', key)
-    workserver.redis.hset('jobs_done', key, value)
+    workserver.hdel('jobs_in_progress', key)
+    workserver.hset('jobs_done', key, value)
     return (True,)
 
 
 @check_for_database
 def get_client_id(workserver):
-    workserver.redis.incr('total_num_client_ids')
+    workserver.incr('total_num_client_ids')
     return True, int(workserver.redis.get('total_num_client_ids'))
 
 
@@ -64,7 +64,7 @@ def create_server():
     '''Create server, add endpoints, and return the server'''
     workserver = redis.Redis()
     try:
-        workserver.redis.keys('*')
+        workserver.keys('*')
     except redis.exceptions.ConnectionError:
         return None
 
