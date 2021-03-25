35691c7ddca08d4bbc1aa38231159085514945bb
Colby Hillman <colby.hillman@gmail.com>
2021-03-25 17:47:09 -0400

    Re-added main in work server

--- src/c21server/work_server/work_server.py
+++ src/c21server/work_server/work_server.py
@@ -95,3 +95,9 @@ def create_server(database):
         return jsonify({'client_id': values[0]}), 200
 
     return workserver
+
+if __name__ == '__main__':
+    server = create_server(redis.Redis())
+    if server is None:
+        print('There is no Redis database to connect to.')
+    else:
