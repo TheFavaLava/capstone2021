0bd580d87740a9b5ed653eeaab5fe95ea1cfa999
Colby Hillman <colby.hillman@gmail.com>
2021-03-25 17:41:29 -0400

    Reverted changes to work server

--- src/c21server/work_server/work_server.py
+++ src/c21server/work_server/work_server.py
@@ -1,8 +1,12 @@
 from flask import Flask, json, jsonify, request
 import redis
 
-app = Flask(__name__)
-server = create_server()
+
+class WorkServer:
+    def __init__(self, redis_server):
+        self.app = Flask(__name__)
+        self.redis = redis_server
+
 
 def get_first_key(data):
     '''
@@ -29,39 +33,39 @@ def check_for_database(func):
 
 
 @check_for_database
-def get_job(server):
-    keys = server.hkeys('jobs_waiting')
+def get_job(workserver):
+    keys = workserver.redis.hkeys('jobs_waiting')
     if len(keys) == 0:
         return False, jsonify({'error': 'There are no jobs available'}), 400
-    value = server.hget('jobs_waiting', keys[0])
-    server.hset('jobs_in_progress', keys[0], value)
-    server.hdel('jobs_waiting', keys[0])
+    value = workserver.redis.hget('jobs_waiting', keys[0])
+    workserver.redis.hset('jobs_in_progress', keys[0], value)
+    workserver.redis.hdel('jobs_waiting', keys[0])
     return True, keys[0].decode(), value.decode()
 
 
 @check_for_database
-def put_results(server, data):
+def put_results(workserver, data):
     key = get_first_key(data)
-    value = server.hget('jobs_in_progress', key)
+    value = workserver.redis.hget('jobs_in_progress', key)
     if value is None:
         body = {'error': 'The job being completed was not in progress'}
         return False, jsonify(body), 400
-    server.hdel('jobs_in_progress', key)
-    server.hset('jobs_done', key, value)
+    workserver.redis.hdel('jobs_in_progress', key)
+    workserver.redis.hset('jobs_done', key, value)
     return (True,)
 
 
 @check_for_database
-def get_client_id(server):
-    server.incr('total_num_client_ids')
-    return True, int(server.get('total_num_client_ids'))
+def get_client_id(workserver):
+    workserver.redis.incr('total_num_client_ids')
+    return True, int(workserver.redis.get('total_num_client_ids'))
 
 
-def create_server():
+def create_server(database):
     '''Create server, add endpoints, and return the server'''
-    server = redis.Redis()
+    workserver = WorkServer(database)
     try:
-        server.keys('*')
+        workserver.redis.keys('*')
     except redis.exceptions.ConnectionError:
         return None
 
@@ -78,14 +82,14 @@ def create_server():
         if data is None:
             body = {'error': 'The body does not contain the results'}
             return jsonify(body), 400
-        error = put_results(server, data)
+        error = put_results(workserver, data)
         if not error[0]:
             return tuple(error[1:])
         return jsonify({'success': 'The job was successfully completed'}), 200
 
     @workserver.app.route('/get_client_id', methods=['GET'])
     def _get_client_id():
-        success, *values = get_client_id(server)
+        success, *values = get_client_id(workserver)
         if not success:
             return tuple(values)
         return jsonify({'client_id': values[0]}), 200
